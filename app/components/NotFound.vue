a1d504250f4a21cd82da157ead4dff9999262d85
Ronald Marske <scyks@ceow.de>
2022-03-30 08:27:49 +0200

    add not Found component for debugging

---
+++ app/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <h3>Not Found</h3>
+    <q-btn :to="{name: 'settings'}" label="open Settings" />
+  </q-page>
+</template>
+<script>
+import { useRouter, useRoute } from 'vue-router';
+
+export default {
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+    console.log(route, router);
+  }
+};
+</script>
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 import Page from './components/Page.vue';
 import Settings from './components/Settings.vue';
 import Clients from './components/Clients.vue';
+import NotFound from './components/NotFound.vue';
 export default [
   {
     path: '/admin/plugins/unifi_presence',
@@ -9,5 +10,6 @@ export default [
       { name: 'settings', path: '', component: Settings },
       { name: 'clients', path: 'clients', component: Clients }
     ]
-  }
+  },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ];
